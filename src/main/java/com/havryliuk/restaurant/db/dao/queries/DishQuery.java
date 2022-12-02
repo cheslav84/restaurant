9096bd9b279d2a998d810ca84886c7223119ec27
Viacheslav Havryliuk <v.cheslav84@gmail.com>
2022-12-02 03:54:00 +0200

    moved category queries into properties file; moved properties initialization to separate class PropertiesLoader

--- src/main/java/com/havryliuk/restaurant/db/dao/queries/DishQuery.java
+++ src/main/java/com/havryliuk/restaurant/db/dao/queries/DishQuery.java
@@ -1,58 +1,13 @@
 package com.havryliuk.restaurant.db.dao.queries;
 
 import com.havryliuk.restaurant.Constants;
-import com.havryliuk.restaurant.db.dao.implemetnation.DishDaoImpl;
-import com.havryliuk.restaurant.exceptions.PropertyInitializationException;
+import com.havryliuk.restaurant.utils.PropertiesLoader;
 import org.apache.log4j.Logger;
-
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Properties;
-
-
-//public enum DishQuery1 {
-//    ADD_DISH("INSERT INTO dish (name) values (?)"),//todo
-//    FIND_DISH_BY_NAME("SELECT * FROM dish d WHERE d.name=?"),
-//    FIND_ALL_ORDERED_BY_NAME("SELECT * FROM dish ORDER BY name"),
-//    FIND_ALL_ORDERED_BY_PRICE("SELECT * FROM dish ORDER BY price"),
-//    FIND_ALL_ORDERED_BY_CATEGORY("SELECT * FROM dish ORDER BY category"),
-//
-//
-//
-////    FIND_ALL_BY_CATEGORY("SELECT d.*, c.name as 'category_name' FROM dish d JOIN category c ON c.name=?"),
-//    FIND_ALL_BY_CATEGORY("SELECT * FROM dish d JOIN category c WHERE c.name=?"),
-//
-//    FIND_DISH_BY_CATEGORY("SELECT * FROM dish d WHERE d.name=?"),
-//
-//
-//    UPDATE_DISH("UPDATE teams SET name=? WHERE id=?"),//todo
-//    //        DELETE_TEAM("DELETE t, ut FROM teams t JOIN users_teams ut WHERE t.id=? AND ut.team_id=?;"),
-//    DELETE_DISH("DELETE FROM teams WHERE id=?"),//todo
-//    DELETE_USERS_IN_TEAM("DELETE FROM users_teams WHERE team_id=?"),//todo
-//    GET_ALL_DISHES("SELECT * FROM teams t ORDER BY t.name"),//todo
-//    GET_TEAMS_BY_USER("SELECT id, name FROM teams t JOIN users_teams ut ON t.id=ut.team_id WHERE ut.user_id=?");//todo
-//
-//    public String QUERY;
-//    private static final Properties properties = new Properties();
-//
-//     static {
-//        try (FileReader fileReader = new FileReader(Constants.DB_FIELDS_SETTING_FILE)) {
-//            properties.load(fileReader);
-//        } catch (IOException e) {
-//            e.printStackTrace(); //todo log
-//        }
-//    }
-//
-//    DishQuery1(String QUERY) {
-//        this.QUERY = QUERY;
-//    }
-//}
 
  public class DishQuery {
      static Logger log = Logger.getLogger(DishQuery.class.getName());
-
-     private static final Properties properties = new Properties();
-
+     private static final Properties properties;
      public static String ADD_DISH;
      public static String FIND_DISH_BY_NAME;
      public static String FIND_ALL_ORDERED_BY_NAME;
@@ -63,18 +18,10 @@ import java.util.Properties;
      public static String  DELETE_DISH;
 
      static {
-         loadProperties();
+         properties = PropertiesLoader.getProperties(Constants.DB_QUERIES_FILE);
          initialiseVariable();
+         log.debug("Database queries for \"Dish\" table have been initialised successfully.");
      }
-
-        private static void loadProperties() throws PropertyInitializationException {
-            try (FileReader fileReader = new FileReader(Constants.DB_QUERIES_FILE)) {
-                properties.load(fileReader);
-            } catch (IOException e) {
-                log.error("Error loading query properties from file " + Constants.DB_QUERIES_FILE, e);
-                throw new PropertyInitializationException();
-            }
-        }
 
         private static void initialiseVariable() {
             ADD_DISH = (String) properties.get("dish.ADD_DISH");
@@ -87,10 +34,4 @@ import java.util.Properties;
             DELETE_DISH = (String) properties.get("dish.DELETE_DISH");
 
         }
-
-    // public static String UPDATE_DISH("UPDATE teams SET name=? WHERE id=?"),//todo
-     // public static String  DELETE_DISH("DELETE FROM teams WHERE id=?"),//todo
-
-
-
  }
---
+++ src/main/java/com/havryliuk/restaurant/utils/PropertiesLoader.java
@@ -0,0 +1,26 @@
+package com.havryliuk.restaurant.utils;
+
+import com.havryliuk.restaurant.Constants;
+import com.havryliuk.restaurant.exceptions.PropertyInitializationException;
+import org.apache.log4j.Logger;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesLoader {
+    static Logger log = Logger.getLogger(PropertiesLoader.class.getName());
+
+
+       public static Properties getProperties (String path) throws PropertyInitializationException {
+           Properties properties = new Properties();
+           try (FileReader fileReader = new FileReader(path)) {
+               properties.load(fileReader);
+           } catch (IOException e) {
+               log.error("Error loading query properties from file " + path, e);
+               throw new PropertyInitializationException();
+           }
+           return properties;
+       }
+
+}
